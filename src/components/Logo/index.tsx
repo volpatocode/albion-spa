8b264508375e5f089c0ffadba2bac71a0014b084
João Volpato <volpatocode@gmail.com>
2022-10-15 12:11:24 -0300

    feat: logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import { Logo } from "./styles";
+
+export default function index() {
+
+  return (
+    <Logo href="/">
+     lupus
+    </Logo>
+  );
+}
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Logo = styled.a`
+  cursor: pointer;
+  color: rgba(0, 0, 0, 0.95);
+  font-size: 2rem;
+  font-family: "Fuzzy Bubbles", cursive;
+`;
