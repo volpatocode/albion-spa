867bf6d5ff7a33991bb6a6447ac6bf662db22608
João Volpato <volpatocode@gmail.com>
2022-10-15 12:11:58 -0300

    feat: home page structure and background

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/Navbar";
+
+import { PageWrapper } from "./styles";
+
+export default function Home() {
+  return (
+    <PageWrapper>
+      <Navbar />
+    </PageWrapper>
+  );
+}
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,2 +1,13 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
+export const PageWrapper = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  background: linear-gradient(
+    to right,
+    rgba(255, 255, 255, 0.95) 0%,
+    rgba(255, 255, 255, 0.95) 30%,
+    rgba(0, 0, 0, 0.95) 30%,
+    rgba(0, 0, 0, 0.95) 100%
+  );
+`;
