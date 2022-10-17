7840f2732a75d9379adf936f9a51c4bd1fa86ad7
João Volpato <volpatocode@gmail.com>
2022-10-17 18:02:05 -0300

    changes: home structure

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,55 +1,19 @@
 import Navbar from "../../components/Navbar";
-import Carousel from "../../components/Carousel";
-import Footer from "../../components/Footer";
 
-import {
-  PageWrapper,
-  Content,
-  Info,
-  Row,
-  Title,
-  Legend,
-  Subtitle,
-  Typography,
-  Divider,
-  Price,
-  BoxButtons, 
-  Button,
-} from "./styles";
+import { PageWrapper, Content, TitleBox, Title } from "./styles";
+import CardList from "../../components/CardList";
 
 export default function Home() {
   return (
     <PageWrapper>
-      <Navbar />
+      <Navbar logo="black" anchor="black" />
       <Content>
-        <Carousel />
-        <Info>
-          <Row>
-            <Title> Chanel Nº5</Title>
-            <Legend>447 reviews</Legend>
-            <Typography>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </Typography>
-            <Divider />
-          </Row>
-          <Row>
-            <Subtitle>Bottle Size:</Subtitle>
-            <Typography>Large</Typography>
-            <Divider />
-          </Row>
-          <Row>
-            <Subtitle>Perfume Type:</Subtitle>
-            <Typography>Nº5</Typography>
-            <Divider />
-            <Price>$199.ºº</Price>
-            <BoxButtons>
-              <Button variant="outlined">Wishlist</Button>
-              <Button variant="contained">Add to Cart</Button>
-            </BoxButtons>
-          </Row>
-        </Info>
+        <TitleBox>
+          <Title>be lupus</Title>
+          <Title style={{alignSelf: "flex-end"}} >be us</Title>
+        </TitleBox>
+        <CardList/>
       </Content>
-      <Footer />
     </PageWrapper>
   );
 }
