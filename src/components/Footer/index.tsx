5aeb47bea406b20199bc2937dea49902f732aab9
João Volpato <volpatocode@gmail.com>
2022-10-25 20:53:30 -0300

    feat: footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,56 @@
-import { Footer } from "./styles";
+import Logo from "../Logo";
+import {
+  Wrapper,
+  Footer,
+  Title,
+  TitleBox,
+  Row,
+  Typography,
+  AnchorList,
+  Anchor,
+  ListTitle,
+  Divider,
+  Copyright,
+} from "./styles";
 
 export default function index() {
-  return <Footer>aa</Footer>;
+  return (
+    <Wrapper>
+      <Footer>
+        <Row style={{ justifyContent: "space-between" }}>
+          <TitleBox>
+            <Title style={{ marginRight: "2rem" }}>Let's work</Title>
+            <Title style={{ alignSelf: "flex-end" }}>together</Title>
+          </TitleBox>
+          <Typography style={{marginTop: "3rem"}}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptatibus aperiam, odio impedit est maxime aliquam quis
+            repudiandae nemo, maiores alias neque molestiae magni voluptatem
+            illum qui, possimus tempore numquam fugit.
+          </Typography>
+        </Row>
+        <Row>
+          <Logo />
+          <AnchorList>
+            <ListTitle>Adress</ListTitle>
+            <Anchor>744 2th Street, 14st Floor, New York, USA</Anchor>
+          </AnchorList>
+          <AnchorList>
+            <ListTitle>Call us on</ListTitle>
+            <Anchor>+1442 4214 2137</Anchor>
+            <Anchor>+1002 9405 0141</Anchor>
+            <Anchor>volpatocode@gmail.com</Anchor>
+          </AnchorList>
+          <AnchorList>
+            <ListTitle>Our Polices</ListTitle>
+            <Anchor>Privacy Policy</Anchor>
+            <Anchor>Terms of Use</Anchor>
+            <Anchor>Refund Policy</Anchor>
+          </AnchorList>
+        </Row>
+      </Footer>
+      <Divider />
+      <Copyright>Copyright &copy; lupus2022. All Right Reserves</Copyright>
+    </Wrapper>
+  );
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,85 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+  background-color: #0d0d0d;
+  padding: 4rem 0;
+`;
+
 export const Footer = styled.div`
-background-color: #0D0D0D;
+  padding: 0 15%;
+  display: flex;
+  flex-direction: column;
+  gap: 10rem;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 2rem;
+`;
+
+export const TitleBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+`;
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  color: rgba(255, 255, 255, 0.9);
+  letter-spacing: 2px;
+`;
+
+export const Subtitle = styled.p`
+  font-weight: 400;
+  font-size: 0.8rem;
+  color: rgba(255, 255, 255, 0.7);
+  line-height: 1.65;
+  max-width: 40%;
+  text-align: justify;
+`;
+
+export const Typography = styled.p`
+  color: rgba(255, 255, 255, 0.7);
+  width: 40%;
+  text-align: justify;
+`;
+
+export const AnchorList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const Anchor = styled.a`
+  cursor: pointer;
+  font-size: 1rem;
+  color: rgba(255, 255, 255, 0.7);
+  :hover{
+    text-decoration: underline;
+  }
+`;
+
+export const ListTitle = styled.p`
+  color: rgba(255, 255, 255, 0.7);
+  font-weight: 600;
+  letter-spacing: 0.3px;
+  padding-bottom: 0.3rem;
+`;
+
+export const Divider = styled.div`
+  background-color: rgba(255, 255, 255, 0.3);
+  width: 100%;
+  height: 1px;
+  margin: 2rem 0 0 0;
+`;
+
+export const Copyright = styled.p`
+display: grid;
+padding: 1rem;
+place-items: center;
+  color: rgba(255, 255, 255, 0.6);
 `;
