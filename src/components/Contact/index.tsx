64548772112fcacb7cae4c8dc7705c9a9a787085
João Volpato <volpatocode@gmail.com>
2022-11-01 20:44:06 -0300

    feat: dynamic contact info component

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,31 @@
+import { Contact } from "./styles";
+import Icon from "@mdi/react";
+import { mdiInstagram } from "@mdi/js";
+import { mdiEmail } from "@mdi/js";
+import { mdiCellphone } from "@mdi/js";
+
+type contactType = {
+  type: string;
+  info: string;
+};
+
+export default function index({ type, info }: contactType) {
+  const icon = {
+    number: mdiInstagram,
+    email: mdiEmail,
+    insta: mdiCellphone,
+  };
+
+  return (
+    <Contact>
+      <Icon
+        //@ts-ignore
+        path={icon[type]}
+        title="Instagram"
+        size={1}
+        color="rgba(255, 255, 255, 0.95)"
+      />
+      {info}
+    </Contact>
+  );
+}
---
+++ src/components/Contact/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Contact = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: .8rem;
+  gap: 0.3rem;
+  border: 1px solid rgba(255, 255, 255, 0.95);
+  color : rgba(255, 255, 255, 0.95);
+  border-radius: 3px;
+`
